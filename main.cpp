bb7b4290fcef75c378756a511148fa5db46a2bb8
parol222333999-sys <parol222333999@gmail.com>
2026-09-08 21:18:37 +0400

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <jni.h>
-#include <GLES3/gl3.h>
-
-extern "C" {
-    JNIEXPORT void JNICALL
-    Java_com_tweaks_utility_MainActivity_nativeInit(JNIEnv* env, jobject thiz) {
-        // Точка входа ядра
-    }
-}
