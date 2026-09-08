4af05fb9f97f659bd86021cb448964fdce640ab3
parol222333999-sys <parol222333999@gmail.com>
2026-09-08 21:02:57 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <jni.h>
+#include <GLES3/gl3.h>
+
+extern "C" {
+    JNIEXPORT void JNICALL
+    Java_com_tweaks_utility_MainActivity_nativeInit(JNIEnv* env, jobject thiz) {
+        // Точка входа ядра
+    }
+}
